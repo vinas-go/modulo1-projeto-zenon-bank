9d44fe618001aa35263f75a0272c571058711996
Vinicius <vinasgo@gmail.com>
2026-04-22 11:48:14 -0300

    feat: adicionar suporte a internacionalização no relatório
    - Implementado uso de Locale e ResourceBundle para internacionalização.
    - Adicionado formatação de números e moeda no relatório.
    - Criados arquivos de propriedades para português e inglês.

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ReportMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ReportMain.java
@@ -1,11 +1,25 @@
 package br.com.zenon.fraud;
 
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class ReportMain {
     public static void main(String[] args) {
+        Locale locale = Locale.of("pt", "BR");
+
+        var integerFormatter = NumberFormat.getIntegerInstance(locale);
+        var currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+        currencyFormatter.setCurrency(Currency.getInstance(Locale.US));
+
+        ResourceBundle report = ResourceBundle.getBundle("report", locale);
+
+
         System.out.println("Iniciando....");
         var transactionReport = new TransactionReport("data/PS_20174392719_1491204439457_log.csv");
-        System.out.println("Total de linhas: " + transactionReport.countLines());
-        System.out.println("Total de fraudes: " + transactionReport.countFrauds());
-        System.out.println("Valor total transacionado: " + transactionReport.totalOfTransactions());
+        System.out.println(report.getString("total_lines") + ": " + integerFormatter.format(transactionReport.countLines()));
+        System.out.println(report.getString("total_frauds") + ": " + integerFormatter.format(transactionReport.countFrauds()));
+        System.out.println(report.getString("total_amount") + ": " + currencyFormatter.format(transactionReport.totalOfTransactions()));
     }
 }
