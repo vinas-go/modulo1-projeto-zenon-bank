0473f9914fa022321390ef74eb31167c00c48f90
Vinicius <vinasgo@gmail.com>
2026-04-22 09:19:30 -0300

    feat: adicionar repositórios para buscar transações por cliente
    - Implementação de `TransactionRepository` e classes concretas `TransactionListRepository` e `TransactionMapRepository` para busca de transações.
    - Adicionado suporte na `TransactionIngestor` para leitura de transações em formato de mapa.
    - Alteração no tamanho máximo de leitura de transações de 50.000 para 100.000.

---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionListRepository.java
@@ -0,0 +1,24 @@
+package br.com.zenon.fraud;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class TransactionListRepository implements TransactionRepository {
+
+    private final List<Transaction> transactions;
+
+    public TransactionListRepository(List<Transaction> transactions) {
+        Objects.requireNonNull(transactions);
+        this.transactions = transactions;
+    }
+
+    @Override
+    public Optional<Transaction> findByName(String originName) {
+        return transactions.stream()
+                .filter(t -> t.origin().name().equals(originName))
+                .findFirst();
+    }
+
+
+}
