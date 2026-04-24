c86e42cb0e876f66107cf8272b414e85631e021b
Vinicius <vinasgo@gmail.com>
2026-04-24 08:29:47 -0300

    refactor: melhorias em conexões e ingestão de dados
    - Ativa chamadas ao TransactionIngestor no DBMain.
    - Simplifica gerenciamento de conexões e statements em TransactionSQLRepository.
    - Renomeia métodos para melhorar legibilidade e consistência.

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/DBMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/DBMain.java
@@ -1,15 +1,14 @@
 package br.com.zenon.fraud;
 
 import java.util.List;
-import java.util.Optional;
 
 public class DBMain {
 
     public static void main(String[] args) {
-        //TransactionIngestor transactionIngestor = new TransactionIngestor();
+        TransactionIngestor transactionIngestor = new TransactionIngestor();
 
-        //String filePath = "data/PS_20174392719_1491204439457_log.csv";
-        //List<Transaction> lista = transactionIngestor.read(filePath, 10_000);
+        String filePath = "data/PS_20174392719_1491204439457_log.csv";
+        List<Transaction> lista = transactionIngestor.read(filePath, 10_000);
 
         TransactionRepository repository = new TransactionSQLRepository();
         //lista.forEach(repository::save);
